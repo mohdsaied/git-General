7926ed632838fe7bf9c8dfe7c75e3e6d5ad62e11
MO SAIED <mosaied00786@gmail.com>
2025-02-17 13:12:00 +0530

    Add method Dog

--- Registration.java
+++ Registration.java
@@ -2,4 +2,8 @@ class Registration{
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public String registerUser() {
+        return "Hello saied";
+    }
 }
