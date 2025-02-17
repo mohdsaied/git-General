83ab21bd6c65dcdcef0b3d9ab2eb6f6db568de1b
MO SAIED <mosaied00786@gmail.com>
2025-02-17 13:14:50 +0530

    Calling method dog in main

--- Registration.java
+++ Registration.java
@@ -1,9 +1,13 @@
 class Registration{
     public static void main(String[] args) {
+        Registration user = new Registration();
+        user.registerUser();
         System.out.println("Hello");
     }
 
     public String registerUser() {
         return "Hello saied";
     }
+
+
 }
